b84d3fa2b312743af83ef4cf651f0d10169407c5
Daisy Aurora <daurora@ucsdedu>
2016-02-25 18:35:20 +0000

    thirdpartyAPI lab

---
+++ public/js/googleMaps.js
@@ -0,0 +1,28 @@
+function initMap() {
+  // Create center marker at UCSD
+  var ucsd_ltlng = {lat:32.885, lng:-117.24131};
+
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: ucsd_ltlng,
+    zoom: 15
+  });
+
+  // This event listener calls addMarker() when the map is clicked.
+  google.maps.event.addListener(map, 'click', function(event) {
+    addMarker(event.latLng, map);
+  });
+
+  // Add a marker at the center of the map.
+  addMarker(ucsd_ltlng, map);
+}
+
+// Adds a marker to the map.
+function addMarker(location, map) {
+  // Add the marker at the clicked location, and add the next-available label
+  // from the array of alphabetical characters.
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+}
